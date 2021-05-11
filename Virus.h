950c3facd050fae1c58e3bf9811f53f80100d94f
Levisoto <levisoto.337@gmail.com>
2021-05-11 15:53:10 -0500

    clean RegistroCaso method

--- Virus.h
+++ Virus.h
@@ -36,19 +36,17 @@ int Coronavirus::ObtenerCantidadInfectdos() { return this->cantidadInfectados; }
 void Coronavirus::RegistrarCaso(vector<string> answers) {
   // cout << "AL entrar  a " << nombre << endl;
   int val = 0;
-  if (answers.size() == caracter.size()) {
-    // cout << "Entro a " << nombre << endl;
-    for (int i = 0; i < answers.size(); i++) {
-      for (int j = 0; j < caracter.size(); j++) {
-        if (answers[i] == caracter[j]) {
-          val++;
-        }
+  // cout << "Entro a " << nombre << endl;
+  for (int i = 0; i < answers.size(); i++) {
+    for (int j = 0; j < caracter.size(); j++) {
+      if (answers[i] == caracter[j]) {
+        val++;
       }
     }
-    if (val == answers.size()) {
-      // cout << "Agregado a " << nombre << endl;
-      cantidadInfectados++;
-    }
+  }
+  if (val == caracter.size() && val == answers.size()) {
+    // cout << "Agregado a " << nombre << endl;
+    cantidadInfectados++;
   }
 }
 string Coronavirus::ObtenerNombre() { return nombre; }
